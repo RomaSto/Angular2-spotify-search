9d00b476b590c30dc956ff1e008fe66a705ac63d
Roma <roma.storoschuk@gmail.com>
2016-11-10 21:43:51 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+# These are the project files for the project and section they are featured in
+
+# Install the dependencies
+
+npm install
+
+# Start the application
+
+npm start
+
---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import { AppComponent }  from './app.component';
+
+import {SearchComponent} from './components/search/search.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {AboutComponent} from './components/about/about.component';
+import {ArtistComponent} from './components/artist/artist.component';
+import {AlbumComponent} from './components/album/album.component';
+
+import {routing} from './app.routing';
+
+@NgModule({
+  imports: [ BrowserModule, 
+              routing, 
+              FormsModule, 
+              HttpModule ],
+  declarations: [ AppComponent,
+                  SearchComponent,
+                  NavbarComponent,
+                  AboutComponent,
+                  ArtistComponent,
+                  AlbumComponent],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
